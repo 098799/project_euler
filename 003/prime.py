3d16517642fd5eec5a3ddd3e4001354494baeecb
098799 <tomek.grining@gmail.com>
2016-12-20 15:47:55 +0100

    One-liner

--- 003/prime.py
+++ 003/prime.py
@@ -1,34 +1,38 @@
-import math
-import time
+from sympy import primefactors
 
-def checkprime(a):
-    for i in range(2,math.ceil(math.sqrt(a+0.001))):
-        if a%i == 0:
-            return False
-    return True
+print(primefactors(600851475143)[-1])
 
-def lprimfactor(a):
-    d = 2
-    lista = []
-    while a > 1:
-        if a%d == 0:
-            lista.append(d)
-            a //= d
-        else:
-            d+=1
+# import math
+# import time
 
-    return lista
+# def checkprime(a):
+#     for i in range(2,math.ceil(math.sqrt(a+0.001))):
+#         if a%i == 0:
+#             return False
+#     return True
 
-    # for i in range(2,math.ceil(a/2+0.1)):
-    #     if a%i == 0:
-    #        if checkprime(i):
-    #            print(i)
-    # return
+# def lprimfactor(a):
+#     d = 2
+#     lista = []
+#     while a > 1:
+#         if a%d == 0:
+#             lista.append(d)
+#             a //= d
+#         else:
+#             d+=1
+
+#     return lista
+
+#     # for i in range(2,math.ceil(a/2+0.1)):
+#     #     if a%i == 0:
+#     #        if checkprime(i):
+#     #            print(i)
+#     # return
         
 
-aa=time.process_time()
-j=600851475143
-#j=13195
-print(lprimfactor(j))
-bb=time.process_time()
-print(bb-aa)
+# aa=time.process_time()
+# j=600851475143
+# #j=13195
+# print(lprimfactor(j))
+# bb=time.process_time()
+# print(bb-aa)
