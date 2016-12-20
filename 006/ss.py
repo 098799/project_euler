1c59bd38c41476fb4a631259bcb0bc822dd8ce82
098799 <tomek.grining@gmail.com>
2016-12-20 15:57:19 +0100

    One-liner

--- 006/ss.py
+++ 006/ss.py
@@ -1,14 +1,16 @@
-def sumsq(n):
-    sum = 0
-    for i in range(1,n+1):
-        sum += i**2
-    return sum
+print(sum(i for i in range(101))**2 - sum(i**2 for i in range(101)))
 
-def sqsum(n):
-    sum = 0
-    for i in range(1,n+1):
-        sum += i
-    return sum**2
+# def sumsq(n):
+#     sum = 0
+#     for i in range(1,n+1):
+#         sum += i**2
+#     return sum
 
-n = 100
-print(sqsum(n)-sumsq(n))
+# def sqsum(n):
+#     sum = 0
+#     for i in range(1,n+1):
+#         sum += i
+#     return sum**2
+
+# n = 100
+# print(sqsum(n)-sumsq(n))
