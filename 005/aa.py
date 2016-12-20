5041dd2a28b50f115a75080887d7374f81b3a1a3
098799 <tomek.grining@gmail.com>
2016-12-20 15:53:39 +0100

    Smartypants-one-liner

--- 005/aa.py
+++ 005/aa.py
@@ -1,17 +1,19 @@
-thelist=[20,19,18,17,16,15,14,13,12,11]
+print(2*3*2*5*7*2*3*11*13*2*17*19)
 
-def checkthething(a):
-    for i in thelist:
-        if a%i != 0:
-            return False
-    return True
+# thelist=[20,19,18,17,16,15,14,13,12,11]
 
-a=1
-found=False
-while not found:
-    if checkthething(a):
-        print(a)
-        for i in range(len(thelist)):
-            print(a/thelist[i])
-        found=True
-    a = a + 1
+# def checkthething(a):
+#     for i in thelist:
+#         if a%i != 0:
+#             return False
+#     return True
+
+# a=1
+# found=False
+# while not found:
+#     if checkthething(a):
+#         print(a)
+#         for i in range(len(thelist)):
+#             print(a/thelist[i])
+#         found=True
+#     a = a + 1
