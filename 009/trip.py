513cd27399ba87d207a734a867401bbf4557e321
098799 <tomek.grining@gmail.com>
2016-12-20 16:03:21 +0100

    Small changes

--- 009/trip.py
+++ 009/trip.py
@@ -1,15 +1,12 @@
-import math
+from math import sqrt
 
 def findtriplet(n):
     for a in range(1,n//2):
-        for b in range(1,n//2):
-            c = math.sqrt(a**2+b**2)
+        for b in range(a,n//2):
+            c = sqrt(a**2+b**2)
             if a+b+c==n:
-                return n,[a,b,int(c)]
+                return int(a*b*c)
     return n,False
 
-nn = 1001
-
-for a in range(1000,nn):
-    print(findtriplet(a))
-    print(findtriplet(a)[1][0]*findtriplet(a)[1][1]*findtriplet(a)[1][2])
+a = 1000
+print(findtriplet(a))
