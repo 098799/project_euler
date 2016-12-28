498c298d99cbf88182afa446aba36bad32ad3058
098799 <tomek.grining@gmail.com>
2016-12-28 14:10:49 +0100

    Next one

---
+++ 021/amicable.py
@@ -0,0 +1,21 @@
+from math import sqrt,floor,ceil
+
+def properDivisor(n):
+    lista = [1]
+    if n>1:
+        for i in range(2,int(sqrt(n))+1):
+            if n%i == 0:
+                lista.append(i)
+                if i != n/i:
+                    lista.append(int(n/i))
+    return lista
+
+totalsum = 0
+
+for i in range(2,10001):
+    val = sum(properDivisor(i))
+    val2 = sum(properDivisor(val))
+    if val2 == i and val != val2:
+        totalsum += val+val2
+
+print(int(totalsum/2))
