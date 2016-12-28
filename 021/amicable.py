e265ff0b538bc3c861f6e980230fb222863879ce
098799 <tomek.grining@gmail.com>
2016-12-28 18:26:35 +0100

    Names more python-y

--- 021/amicable.py
+++ 021/amicable.py
@@ -1,18 +1,20 @@
-from math import sqrt,floor,ceil
+from math import sqrt
+
 
 def properDivisor(n):
     lista = [1]
-    if n>1:
-        for i in range(2,int(sqrt(n))+1):
-            if n%i == 0:
+    if n > 1:
+        for i in range(2, int(sqrt(n))+1):
+            if n % i == 0:
                 lista.append(i)
                 if i != n/i:
                     lista.append(int(n/i))
     return lista
 
+
 totalsum = 0
 
-for i in range(2,10001):
+for i in range(2, 10001):
     val = sum(properDivisor(i))
     val2 = sum(properDivisor(val))
     if val2 == i and val != val2:
--- 022/nam.py
+++ 022/nam.py
@@ -1,33 +1,15 @@
-# f = open('p022_names.txt','r')
-# full=f.read().split('","')
-# full[0] = full[0][1:]
-# full[-1] = full[-1][:-1]
+from csv import reader
+results = reader(open('p022_names.txt'))
 
-# xxx = open('x.dat','w')
-# for i in range(len(full)):
-#     xxx.write(full[i])
-#     xxx.write("\n")
+res = sorted(next(results))
 
-# sfull = sorted(full)
+alphabet = {'A':1 , 'B':2, 'C':3, 'D':4, 'E':5, 'F':6, 'G':7, 'H':8, 'I':9, 'J':10, 'K':11, 'L':12, 'M':13, 'N':14, 'O':15, 'P':16, 'Q':17, 'R':18, 'S':19, 'T':20, 'U':21, 'V':22, 'W':23, 'X':24, 'Y':25, 'Z':26}
 
-ff = open('sortnames.txt','r')
-sfull = ff.read().splitlines()
+counter = 0
+totalsum = 0
+for name in res:
+    counter += 1
+    partialsum = sum(alphabet[i] for i in name)*counter
+    totalsum += partialsum
 
-alphabet = [ 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z' ]
-
-def val(a):
-    return alphabet.index(str(a))+1
-
-def alpval(a):
-    sum = 0
-    for i in range(len(a)):
-        sum += val(a[i])
-    return sum
-
-sum = 0
-
-for i in range(len(sfull)):
-    sum += (i+1)*alpval(sfull[i])
-    print(sfull[i],i+1,alpval(sfull[i]))
-
-print(sum)
+print(totalsum)
