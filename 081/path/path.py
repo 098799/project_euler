6206a4f1197286c7382b391f40726a469eb5d150
098799 <tomek.grining@gmail.com>
2018-04-02 21:44:14 +0200

    081 Done

--- 081/path/path.py
+++ 081/path/path.py
@@ -4,3 +4,28 @@ def matrix_cut(matrix, n):
         return [matrix[n-i-1][i] for i in range(n) if (n-i-1 < matrix_length and i < matrix_length)]
     else:
         raise ValueError("Too big for this matrix")
+
+
+def add_path(previous_cut, next_cut):
+    if len(next_cut) > len(previous_cut):
+        borders_enabled = True
+        offset = -1
+    else:
+        borders_enabled = False
+        offset = +1
+    for number, item in enumerate(next_cut):
+        if (number == 0 and borders_enabled):
+            next_cut[number] += previous_cut[number]
+        elif (number == len(next_cut)-1 and borders_enabled):
+            next_cut[number] += previous_cut[-1]
+        else:
+            next_cut[number] += min(previous_cut[number], previous_cut[number+offset])
+    return next_cut
+
+
+def go_over(matrix):
+    prev_cut = matrix_cut(matrix, 1)
+    for number in range(2, 2*len(matrix)):
+        next_cut = matrix_cut(matrix, number)
+        prev_cut = add_path(prev_cut, next_cut)
+    return next_cut
--- 081/tests/test_path.py
+++ 081/tests/test_path.py
@@ -21,3 +21,33 @@ def test_matrix_cut_big():
 def test_matrix_cut_over():
     with pytest.raises(ValueError):
         path.matrix_cut(test_matrix, 16)
+
+
+def test_add_path_small():
+    prev_cut = path.matrix_cut(test_matrix, 1)
+    next_cut = path.matrix_cut(test_matrix, 2)
+    assert path.add_path(prev_cut, next_cut) == [673+131, 201+131]
+
+
+def test_add_path_big():
+    prev_cut = path.matrix_cut(test_matrix, 7)
+    next_cut = path.matrix_cut(test_matrix, 8)
+    assert path.add_path(prev_cut, next_cut) == [956+111, 37+121]
+
+
+def test_go_over():
+    assert path.go_over(test_matrix) == [2427]
+
+
+def test_final():
+    with open("tests/matrix.txt") as infile:
+        raw_input_matrix = infile.read()
+    big_matrix = []
+    for line in (raw_input_matrix.split("\n"))[:-1]:
+        big_matrix.append(line.split(","))
+
+    for n1, line in enumerate(big_matrix):
+        for n2, item in enumerate(line):
+            big_matrix[n1][n2] = int(item)
+
+    print(path.go_over(big_matrix))  # Final project Euler answer is 427337.
